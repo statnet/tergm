94f158d510fa3d90a60df141c4da3409643a4782
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-08-01 22:45:37 +0000

    Many changes to tergm's startup/citation message: * Converted the DESCRIPTION author list to the Author@R format. * Converted the inst/CITATION file to use information from DESCRIPTION. (It can be transplanted to any other package with only minor changes, and it'll adjust automatically. * Rewrote the startup message code, adding a function mkStartupMessage(pkgname), which automatically constructs the message from the package DESCRIPTION.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tergm/trunk@11592 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,34 @@
-.onAttach <- function(lib, pkg){
-  info <- packageDescription("tergm")
-  packageStartupMessage(
-    paste('\nergm: version ', info$Version, ', created on ', info$Date, '\n',
-          "Copyright (c) 2003, Mark S. Handcock, University of California-Los Angeles\n",
-          "                    David R. Hunter, Penn State University\n",
-          "                    Carter T. Butts, University of California-Irvine\n",
-          "                    Steven M. Goodreau, University of Washington\n",
-          "                    Pavel N. Krivitsky, Penn State University\n",
-          "                    Martina Morris, University of Washington\n",
+mkStartupMessage <- function(pkgname){
+  INST_MAP <- list(washington.edu="University of Washington",
+                   uw.edu="University of Washington",
+                   psu.edu="Penn Statue University",
+                   uci.edu="University of California -- Irvine",
+                   ucla.edu="University of California -- Los Angeles",
+                   nyu.edu="New York University") 
+
+  desc <- packageDescription(pkgname)
+  pns <- eval(parse(text=desc$`Authors@R`))
+  pnnames <- format(pns, include=c("given","family"))
+  pninsts <- sapply(pns, function(pn) NVL(INST_MAP[[gsub(".*?([^.@]+\\.[^.]{2,4})$","\\1",pn$email)]],""))
+
+  authors <- sapply(pns, function(pn) "aut" %in% pn$role)
+
+  pnlines <- ifelse(pninsts=="", pnnames, paste(pnnames,pninsts, sep=", "))
+  
+  copylist <- paste("Copyright (c) ",substr(desc$Date,1,4),", ",sep="")
+  copylist <- paste(copylist, pnlines[authors][1],"\n",
+                    paste(
+                      paste(rep(" ",nchar(copylist)),collapse=""),
+                      c(pnlines[authors][-1],if(sum(!authors)) "with contributions from",pnlines[!authors]),sep="",collapse="\n"),
+                    sep="") 
+     paste("\n",desc$Package,": version ", desc$Version, ', created on ', desc$Date, '\n',copylist,"\n",
           'Based on "statnet" project software (statnet.org).\n',
           'For license and citation information see statnet.org/attribution\n',
-          'or type citation("ergm").\n', sep="")
-                        )
+          'or type citation("',desc$Package,'").\n', sep="")
+}
+
+.onAttach <- function(lib, pkg){
+  packageStartupMessage(mkStartupMessage("tergm"))
   
   .RegisterMHPs()
   .RegisterConstraintImplications()
