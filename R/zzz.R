020e3cdabfe692802e1ea2f704276d635920592b
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-08-03 06:17:53 +0000

    * Added statnet.common to tergm dependencies. * Removed mkStartupMessage from tergm, since it's in statnet.common now. * CITATION now uses the statnet.common mechanism.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tergm/trunk@11602 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/zzz.R
+++ R/zzz.R
@@ -1,32 +1,3 @@
-mkStartupMessage <- function(pkgname){
-  INST_MAP <- list(washington.edu="University of Washington",
-                   uw.edu="University of Washington",
-                   psu.edu="Penn Statue University",
-                   uci.edu="University of California -- Irvine",
-                   ucla.edu="University of California -- Los Angeles",
-                   nyu.edu="New York University") 
-
-  desc <- packageDescription(pkgname)
-  pns <- eval(parse(text=desc$`Authors@R`))
-  pnnames <- format(pns, include=c("given","family"))
-  pninsts <- sapply(pns, function(pn) NVL(INST_MAP[[gsub(".*?([^.@]+\\.[^.]{2,4})$","\\1",pn$email)]],""))
-
-  authors <- sapply(pns, function(pn) "aut" %in% pn$role)
-
-  pnlines <- ifelse(pninsts=="", pnnames, paste(pnnames,pninsts, sep=", "))
-  
-  copylist <- paste("Copyright (c) ",substr(desc$Date,1,4),", ",sep="")
-  copylist <- paste(copylist, pnlines[authors][1],"\n",
-                    paste(
-                      paste(rep(" ",nchar(copylist)),collapse=""),
-                      c(pnlines[authors][-1],if(sum(!authors)) "with contributions from",pnlines[!authors]),sep="",collapse="\n"),
-                    sep="") 
-     paste("\n",desc$Package,": version ", desc$Version, ', created on ', desc$Date, '\n',copylist,"\n",
-          'Based on "statnet" project software (statnet.org).\n',
-          'For license and citation information see statnet.org/attribution\n',
-          'or type citation("',desc$Package,'").\n', sep="")
-}
-
 .onAttach <- function(lib, pkg){
   packageStartupMessage(mkStartupMessage("tergm"))
   
