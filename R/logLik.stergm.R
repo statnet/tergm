18a513b51d65499bb040ec7993a204dafa7d6440
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-08-17 07:16:53 +0000

    * Cleaned up logLik.stergm(). * Added and documented logLikNull.stergm().
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tergm/trunk@11654 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/logLik.stergm.R
+++ R/logLik.stergm.R
@@ -6,18 +6,31 @@ logLik.stergm<-function(object, add=FALSE, force.reeval=FALSE, eval.loglik=add |
   if(!is.null(control$seed))  set.seed(as.integer(control$seed))
 
   if(add){
-    object$formation.fit <- logLik(object$formation.fit, add=TRUE, force.reeval=FALSE, eval.loglik = add || force.reeval, control=control$control.form)
-    object$dissolution.fit <- logLik(object$dissolution.fit, add=TRUE, force.reeval=FALSE, eval.loglik = add || force.reeval, control=control$control.diss)
+    object$formation.fit <- logLik(object$formation.fit, add=add, force.reeval=force.reeval, eval.loglik = add || force.reeval, control=control$control.form)
+    object$dissolution.fit <- logLik(object$dissolution.fit, add=add, force.reeval=force.reeval, eval.loglik = add || force.reeval, control=control$control.diss)
     
     object
   }else{
-    llk.form <- logLik(object$formation.fit, add=FALSE, force.reeval=FALSE, eval.loglik = add || force.reeval, control=control$control.form)
-    llk.diss <- logLik(object$dissolution.fit, add=FALSE, force.reeval=FALSE, eval.loglik = add || force.reeval, control=control$control.diss)
+    llk.form <- logLik(object$formation.fit, add=add, force.reeval=force.reeval, eval.loglik = eval.loglik, control=control$control.form)
+    llk.diss <- logLik(object$dissolution.fit, add=add, force.reeval=force.reeval, eval.loglik = eval.loglik, control=control$control.diss)
 
     llk <- llk.form + llk.diss
+    class(llk) <- "logLik"
     attr(llk,"df") <- attr(llk.form,"df") + attr(llk.diss,"df")
     attr(llk,"nobs") <- attr(llk.form,"nobs") + attr(llk.diss,"nobs")
     
     llk
   }
+}
+
+logLikNull.stergm <- function(object, control=control.logLik.stergm(), ...){
+    llk.form <- logLikNull(object$formation.fit, control=control$control.form)
+    llk.diss <- logLikNull(object$dissolution.fit, control=control$control.diss)
+
+    llk <- llk.form + llk.diss
+    class(llk) <- "logLik"
+    attr(llk,"df") <- attr(llk.form,"df") + attr(llk.diss,"df")
+    attr(llk,"nobs") <- attr(llk.form,"nobs") + attr(llk.diss,"nobs")
+    
+    llk
 }
