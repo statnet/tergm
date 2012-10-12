fd2c5542bc38846ab5ac45fabec1003a94298343
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-10-12 19:52:42 +0000

    Added three utility functions mainly for interfacing between networkDynamic and network+lasttoggle: * network.extract.with.lasttoggle(nwd, at): a wrapper around network.extract() that also sets class to just the plain "network", removes the "active" attributes from the extracted cross-section, stores the index of each vertex in the original networkDynamic, and sets the lasttoggle information based on the changes prior to at. * to.networkDynamic.lasttoggle(nw): attempts to construct a networkDynamic from lasttoggle information, by setting the onset time of each edge in nw implied by lasttoggle; essentially the inverse of network.extract.with.lasttoggle(). * networkDynamic.apply.changes(nwd, changes): given a networkDynamic and a matrix of changes (time, tail, head, to), applies these changes to the networkDynamic as efficiently as possible; assumes that the networkDynamic's latest toggles are prior to, or at the same time as, the toggles in the change matrix. (This function should probably be incorporated into networkDynamic in some form.)
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tergm/trunk@11801 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/stergm.utils.R
+++ R/stergm.utils.R
@@ -27,7 +27,6 @@ get.dev <- local({
 # A customized level plot that uses cyan for positive values, red for
 # negative, white for close to 0, black for exactly 0, and ensures
 # that the scales on both sides are the same.
-
 .my.levelplot <- function(m,levels=80,...){
   bound <- max(na.omit(c(abs(m))))
 
@@ -36,4 +35,103 @@ get.dev <- local({
             col.regions=c(hsv(h=0,s=seq(1,2/levels,length.out=levels/2),v=1),rgb(0,0,0),
               hsv(h=.5,s=seq(2/levels,1,length.out=levels/2),v=1)),
             ...)
+}
+
+
+# A wrapper around network.extract
+network.extract.with.lasttoggle <- function(nwd, at){
+  nw <- network.extract(nwd, at=at)
+  nw %v% ".networkDynamicID" <- which(is.active(nwd, at=at, v=seq_len(network.size(nwd))))
+  
+  # There is probably a more efficient way to do this:
+
+  # Note that nw is still a networkDynamic, and still has vertex and
+  # edge activity.
+  lttails <- lapply(nw$mel, "[[", "outl")
+  ltheads <- lapply(nw$mel, "[[", "inl")
+  # I.e., from the edge attribute list, grab the "active" matrix, and from it, extract the highest (most recent) timestamp that's prior to or at at.
+  # Note that if x[x<=at] is empty, max(x[x<=at]) returns -Inf, which is what we want.
+  ltlts <- lapply(lapply(lapply(nw$mel, "[[", "atl"), "[[", "active"), function(x) suppressWarnings(max(x[x<=at])))
+
+  # Now, strip the networkDynamic information from it.
+  delete.vertex.attribute(nw, "active")
+  delete.edge.attribute(nw, "active")
+  class(nw) <- "network"
+  
+  ltm <-
+    if(is.bipartite(nw)) m <- matrix(-Inf, nw%n%"bipartite", network.size(nw) - nw%n%"bipartite")
+    else m <- matrix(-Inf, network.size(nw), network.size(nw))
+
+  for(i in seq_along(ltlts))
+    if(ltlts[[i]]!=-Inf){
+      e<-c(lttails[[i]],ltheads[[i]])
+      if(!all(e)) next
+      if(!is.directed(nw)) e <- c(min(e),max(e))
+      if(is.bipartite(nw)) e[2] <- e[2] - nw %n% "bipartite"
+      m[e[1],e[2]] <- ltlts[[i]]
+    }
+  m[m==-Inf] <- round(-.Machine$integer.max/2)
+  
+  nw %n% "time" <- at
+  nw %n% "lasttoggle" <- to.lasttoggle.matrix(m, is.directed(nw), is.bipartite(nw))
+
+  nw
+}
+
+to.networkDynamic.lasttoggle <- function(nw){
+  nwd <- nw
+  if(!is.null(nw %n% "lasttoggle")){
+
+    lt.edges <- ergm.el.lasttoggle(nw)
+
+    lt.edges <- lt.edges[lt.edges[,3]>round(-.Machine$integer.max/2),,drop=FALSE] 
+
+    if(nrow(lt.edges)) nwd <- deactivate.edges(nwd, onset=-Inf, terminus=lt.edges[,3], e=apply(lt.edges[,1:2,drop=FALSE],1,function(e) get.edgeIDs(nw, e[1], e[2])))
+  }
+  nwd<-delete.network.attribute(nwd, "time")
+  nwd<-delete.network.attribute(nwd, "lasttoggle")
+  class(nwd) <- c("networkDynamic","network")
+  attr(nwd,"end") <- nw %n% "time"
+  
+  nwd
+}
+
+networkDynamic.apply.changes <- function(nwd, changes){
+  ## Add edges that were never present in the initial network.
+  extant.edges <- as.edgelist(nwd)
+  changed.edges <- unique(changes[,c("tail","head"),drop=FALSE])
+  new.edges <- changed.edges[!(paste(changed.edges[,1],changed.edges[,2]) %in% paste(extant.edges[,1],extant.edges[,2])),]
+  nwd <- add.edges(nwd,as.list(new.edges[,1]),as.list(new.edges[,2]))
+  
+  for(i in seq_len(nrow(changes))){
+    eID <- get.edgeIDs(nwd,changes[i,"tail"],changes[i,"head"])
+    # The following two lines are the "correct" way to do this, but
+    # since we know the direction, and since we can assume that
+    # everything after attr(nwd,"end") is "censored", we can do it
+    # faster.
+    #
+    # if(changes[i,"to"]==0) nwd <- deactivate.edges(nwd, onset=changes[i,"time"], terminus=+Inf, e=eID)
+    # if(changes[i,"to"]==1) nwd <- activate.edges(nwd, onset=changes[i,"time"], terminus=+Inf, e=eID)
+    
+    if(changes[i,"to"]==0){
+      # If we are dissolving a tie, we are changing the bottom-right
+      # cell in the spell matrix. However, we can't assume that a
+      # spell matrix exists for a given edge, so we need to check, and
+      # add a (-Inf,time) row if it doesn't.
+      am <- nwd$mel[[eID]]$atl$active
+      if(is.null(am)) am <- rbind(c(-Inf,+Inf))
+      am[nrow(am),2] <- changes[i,"time"]
+      nwd[["mel"]][[eID]]$atl$active <- am
+    }else if(changes[i,"to"]==1){
+      # If we are forming a tie, we are adding a new row.  If (and
+      # this shouldn't happen), there is a spell ending at the same
+      # time as we are trying to start it, merge it in instead.
+      am <- nwd[["mel"]][[eID]]$atl$active
+      if(!is.null(am) && am[nrow(am),2]==changes[i,"time"]) am[nrow(am),2] <- +Inf
+      else am <- rbind(am, c(changes[i,"time"],+Inf))
+    }
+    nwd[["mel"]][[eID]]$atl$active <- am
+  }
+
+  nwd
 }
