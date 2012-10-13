fd2644811ee1d80dc87d225548c91e6e25a08aec
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-10-13 22:45:11 +0000

    Wrote a short simulation test to test consistency of simulate.network(), simulate.networkDynamic(), tergm.godfather() with networkDynamic, tergm.godfather() with change list, and summary.networkDynamic(), for directed, undirected, and bipartite networks.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tergm/trunk@11819 9cb281f4-553f-406f-8d7f-11f072bbfe46

---
+++ tests/sim_gf_sum.R
@@ -0,0 +1,69 @@
+library(tergm)
+library(coda)
+
+logit<-function(p)log(p/(1-p))
+
+coef.form.f<-function(coef.diss,density) -log(((1+exp(coef.diss))/(density/(1-density)))-1)
+
+simtest <- function(S, edges, dur, n, dir=FALSE, bip=0){
+  m <- if(bip) bip else n
+  
+  target.stats<-edges
+  coef.diss<-logit(1-1/dur)
+  
+  dc <- matrix(rnorm(n*m),n,m)
+  if(!dir && !bip) dc <- dc+t(dc)
+  # Note that if nw1!=nw2, then
+  # summary(nw1~edgecov(dc)+edgecov.ages(dc))!=summary(nw2~edgecov(dc)+edgecov.ages(dc))
+  # with probability 1. Thus, they offer a sort of a checksum of a network.
+  
+  g0<-network.initialize(n, dir=dir, bipartite=bip)
+  g0 %n% "dc" <- dc
+  g0 %n% "time" <- 0
+  g0 %n% "lasttoggle" <- -1-rgeom(network.dyadcount(g0),1/4)
+
+
+  
+  dyads <- network.dyadcount(g0)
+  density<-edges/dyads
+  
+  coef.form<-coef.form.f(coef.diss,density)
+  
+  # Get a reasonably close starting network.
+  g1<-san(g0~edges,target.stats=target.stats,verbose=TRUE)
+  
+  print(coef.form)
+  print(coef.diss)
+  
+  # Simulate. Starting from an ordinary network:
+  dynsim<-simulate(g1,formation=~edges,dissolution=~edges,coef.form=coef.form,coef.diss=coef.diss,time.slices=S,verbose=TRUE,monitor=~edgecov("dc")+edgecov.ages("dc"))
+  
+  # Resuming from a networkDynamic:
+  dynsim2<-simulate(dynsim,formation=~edges,dissolution=~edges,coef.form=coef.form,coef.diss=coef.diss,time.slices=S,verbose=TRUE,monitor=~edgecov("dc")+edgecov.ages("dc"))
+  
+  # Resuming from a resumed networkDynamic:
+  dynsim3<-simulate(dynsim2,formation=~edges,dissolution=~edges,coef.form=coef.form,coef.diss=coef.diss,time.slices=S,verbose=TRUE,monitor=~edgecov("dc")+edgecov.ages("dc"))
+
+  sim.stats <- attr(dynsim3,"stats")
+  
+  # Replay the simulation using a networkDynamic:
+  gf1.stats <- as.matrix(tergm.godfather(dynsim3~edgecov("dc")+edgecov.ages("dc"), start=0, end=S*3))
+  
+  # Replay the simulation using a network + list of changes:
+  gf2.stats <- as.matrix(tergm.godfather(g1~edgecov("dc")+edgecov.ages("dc"), start=0, end=S*3, changes=attr(dynsim3,"changes")))
+  
+  # Evaluate summary statistics directly:
+  summ.stats <- summary(dynsim3~edgecov("dc")+edgecov.ages("dc"), at=1:(S*3))
+  
+  # If they aren't all identical, we are in trouble.
+  stopifnot(all.equal(sim.stats,gf1.stats),
+            all.equal(sim.stats,gf2.stats),
+            all.equal(sim.stats,summ.stats))
+}
+
+cat("Undirected, unipartite:\n")
+simtest(20, 10, 10, 10, FALSE, 0)
+cat("Directed, unipartite:\n")
+simtest(20, 10, 10, 10, TRUE, 0)
+cat("Undirected, biipartite:\n")
+simtest(20, 10, 10, 10, FALSE, 3)
