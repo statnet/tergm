71d3d64e13aec912a709e8df40d5193ad5b1398b
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-11-27 21:37:19 +0000

    Transitioned trunk and release branches of ergm and packages that depend on it (ergm.userterms, ergm.count, and tergm) to the stubs+LinkingTo framework: * Added symlinks to header files for exported data structures and functions to ergm/*/inst/include/, and constructed a stub file ergm_stubs.c for function stubs. The header files in question are: changestat.h, edgelist.h, edgetree.h, ergm_stubs.c, MCMC.h, MHproposal.h, model.h, wtchangestat.h, wtedgetree.h, wtMCMC.h, wtMHproposal.h, and wtmodel.h . * Removed header files and stubs from the dependent package sources, replacing them with a single C file (named include_stubs.c) that does nothing but include ergm_stubs.c .
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tergm/trunk@11866 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- src/changestat_stubs.c
+++
@@ -1,7 +0,0 @@
-#include "changestat.h"
-#include "R_ext/Rdynload.h"
-double my_choose(double n, int r){
-static double (*fun)(double,int) = NULL;
-if(fun==NULL) fun = (double (*)(double,int)) R_FindSymbol("my_choose", "ergm", NULL);
-return fun(n,r);
-}
