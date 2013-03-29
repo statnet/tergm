3982073b1e6f8e773ad9beb8fc4fe5bda4238e48
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-03-29 05:23:01 +0000

    Moved the imputation code into its own function, impute.network.list(), and documented it. (This is necessary to be able to reproduce the example in Krivitsky and Handcock (2013).)
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tergm/branches/3.1@11926 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/control.stergm.R
+++ R/control.stergm.R
@@ -22,7 +22,8 @@ control.stergm<-function(init.form=NULL,
                          CMLE.control.form=control.ergm(init=init.form, MCMC.burnin=MCMC.burnin, MCMC.interval=MCMC.interval, MCMC.prop.weights=MCMC.prop.weights.form, MCMC.prop.args=MCMC.prop.args.form, MCMC.init.maxedges=MCMC.init.maxedges, MCMC.packagenames=MCMC.packagenames, parallel=parallel, parallel.type=parallel.type, parallel.version.check=parallel.version.check, force.main=force.main),
                          CMLE.control.diss=control.ergm(init=init.diss, MCMC.burnin=MCMC.burnin, MCMC.interval=MCMC.interval, MCMC.prop.weights=MCMC.prop.weights.diss, MCMC.prop.args=MCMC.prop.args.diss, MCMC.init.maxedges=MCMC.init.maxedges, MCMC.packagenames=MCMC.packagenames, parallel=parallel, parallel.type=parallel.type, parallel.version.check=parallel.version.check, force.main=force.main),
 
-                         CMLE.NA.impute="stop",
+                         CMLE.NA.impute=c(),
+                         CMLE.term.check.override=FALSE,
                          
                          EGMME.main.method=c("Gradient-Descent"),
                          
