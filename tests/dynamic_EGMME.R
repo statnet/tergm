940287d6c4f023c77533d22e58343e534fec1519
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-08-19 09:05:05 +0000

    * Added a dynamic EGMME test for a case available in closed form. * Tweaked the "correct" EGMME value in the other dynamic EGMME test.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/tergm/trunk@12130 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/dynamic_EGMME.R
+++ tests/dynamic_EGMME.R
@@ -10,7 +10,7 @@ target.stats<-c(      n*1/2,    n*0.6,       20)
 # Get a deliberately bad starting network.
 g1<-san(g0~meandeg+degree(1),target.stats=target.stats[-3],verbose=TRUE)
 
-coef.form <- c(-6.801597, 1.016334)
+coef.form <- c(-6.57, 1.01)
 coef.diss <- c(2.944439)
 
 # Fit the model with very poor starting values.
@@ -20,7 +20,7 @@ dynfit<-stergm(g1,formation=~edges+degree(1),dissolution=~offset(edges), targets
 print(summary(dynfit))
 mcmc.diagnostics(dynfit)
 
-stopifnot(all.equal(c(coef.form,coef.diss),unlist(coef(dynfit)),tol=0.05,check.attributes=FALSE))
+stopifnot(all.equal(c(coef.form,coef.diss),unlist(coef(dynfit)),tol=0.01,check.attributes=FALSE))
 
 # All parameters free, edges, degree(1), and edge.ages as target.
 set.seed(1)
@@ -29,5 +29,5 @@ dynfit2<-stergm(g1,formation=~edges+degree(1),dissolution=~edges, targets=~edges
 print(summary(dynfit2))
 mcmc.diagnostics(dynfit2)
 
-stopifnot(all.equal(c(coef.form,coef.diss),unlist(coef(dynfit2)),tol=0.05,check.attributes=FALSE))
+stopifnot(all.equal(c(coef.form,coef.diss),unlist(coef(dynfit2)),tol=0.01,check.attributes=FALSE))
 }, "EGMME")
