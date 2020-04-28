3d31c16d0b20c603ac68177cc086c58b28d8e1ca
chad-klumb <cklumb@gmail.com>
2020-04-28 06:46:46 -0700

    Update print.tergm.R

--- R/print.tergm.R
+++ R/print.tergm.R
@@ -1,4 +1,4 @@
-#  File R/print.stergm.R in package tergm, part of the Statnet suite
+#  File R/print.tergm.R in package tergm, part of the Statnet suite
 #  of packages for network analysis, https://statnet.org .
 #
 #  This software is distributed under the GPL-3 license.  It is free,
