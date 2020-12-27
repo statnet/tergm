aaa175c3596bfe163c9e0fedd7bf9f0996f66181
chad-klumb <cklumb@gmail.com>
2020-12-27 10:23:01 -0800

    add more simulate tests
    and update EoLs
    
    references statnet/tergm-private#136

--- tests/testthat/test-simulate.R
+++ tests/testthat/test-simulate.R
@@ -9,7 +9,6 @@
 #######################################################################
 
 test_that("simulate_formula.network behaves reasonably", {
-
   nw <- network.initialize(100, directed = FALSE)
 
   ## if we don't specify 'dynamic=TRUE' we should go to ergm simulation by default
@@ -97,6 +96,76 @@ test_that("simulate_formula.network behaves reasonably", {
   for(i in seq_len(NROW(el45))) {
     lt_time <- lt[lt[,1] == el45[i,1] & lt[,2] == el45[i,2],3]
     expect_true(identical(lt_time, 4L) || identical(lt_time, 5L))
-  }  
+  }
+})
+
+test_that("simulate_formula.networkDynamic behaves reasonably", {
+  ## do some matching sims with sim_f.nw and sim_f.nwD and then compare times, edgelists, and lasttoggles
+  set.seed(0)
+  nw <- network.initialize(100, directed = FALSE)
+  nw1 <- simulate(nw ~ edges, coef = c(-3), output = "final", dynamic = TRUE)
+  nw2 <- simulate(nw1 ~ Form(~edges) + Diss(~edges), coef = c(-3, 1), output = "final", dynamic = TRUE)
+  nw4 <- simulate(nw2 ~ Form(~edges) + Diss(~edges), coef = c(-3, 1), output = "final", time.slices = 2, dynamic = TRUE)
+  nw5 <- simulate(nw2 ~ Form(~edges) + Diss(~edges), coef = c(-3, 1), output = "final", time.start = 3, time.slices = 2, dynamic = TRUE)
   
+  set.seed(0)
+  nw <- network.initialize(100, directed = FALSE)
+  nwd1 <- simulate(nw ~ edges, coef = c(-3), dynamic = TRUE)
+  nwd2 <- simulate(nwd1 ~ Form(~edges) + Diss(~edges), coef = c(-3, 1), dynamic = TRUE)
+  nwd4 <- simulate(nwd2 ~ Form(~edges) + Diss(~edges), coef = c(-3, 1), time.slices = 2, dynamic = TRUE)
+  nwd5 <- simulate(nwd2 ~ Form(~edges) + Diss(~edges), coef = c(-3, 1), time.start = 3, time.slices = 2, dynamic = TRUE)
+  
+  expect_identical(nw1 %n% "time", as.integer(.get.last.obs.time(nwd1)))
+  expect_identical(nw2 %n% "time", as.integer(.get.last.obs.time(nwd2)))
+  expect_identical(nw4 %n% "time", as.integer(.get.last.obs.time(nwd4)))
+  expect_identical(nw5 %n% "time", as.integer(.get.last.obs.time(nwd5)))
+
+  nwlt1 <- nw1 %n% "lasttoggle"
+  nwlt2 <- nw2 %n% "lasttoggle"
+  nwlt4 <- nw4 %n% "lasttoggle"
+  nwlt5 <- nw5 %n% "lasttoggle"
+  
+  nwel1 <- as.edgelist(nw1)
+  nwel2 <- as.edgelist(nw2)
+  nwel4 <- as.edgelist(nw4)
+  nwel5 <- as.edgelist(nw5)
+  
+  nw_from_nwd1 <- network.extract.with.lasttoggle(nwd1, at=1, TRUE)
+  nw_from_nwd2 <- network.extract.with.lasttoggle(nwd2, at=2, TRUE)
+  nw_from_nwd4 <- network.extract.with.lasttoggle(nwd4, at=4, TRUE)
+  nw_from_nwd5 <- network.extract.with.lasttoggle(nwd5, at=5, TRUE)
+
+  nwdlt1 <- nw_from_nwd1 %n% "lasttoggle"
+  nwdlt2 <- nw_from_nwd2 %n% "lasttoggle"
+  nwdlt4 <- nw_from_nwd4 %n% "lasttoggle"
+  nwdlt5 <- nw_from_nwd5 %n% "lasttoggle"
+
+  nwdel1 <- as.edgelist(nw_from_nwd1)
+  nwdel2 <- as.edgelist(nw_from_nwd2)
+  nwdel4 <- as.edgelist(nw_from_nwd4)
+  nwdel5 <- as.edgelist(nw_from_nwd5)
+  
+  expect_identical(nwel1, nwdel1)  
+  expect_identical(nwel2, nwdel2)  
+  expect_identical(nwel4, nwdel4)  
+  expect_identical(nwel5, nwdel5)  
+  
+  expect_identical(nwlt1[order(nwlt1[,1], nwlt1[,2]),,drop=FALSE], nwdlt1[order(nwdlt1[,1], nwdlt1[,2]),,drop=FALSE])  
+  expect_identical(nwlt2[order(nwlt2[,1], nwlt2[,2]),,drop=FALSE], nwdlt2[order(nwdlt2[,1], nwdlt2[,2]),,drop=FALSE])  
+
+  ## these next two should only include non-edges if they were toggled off on the most 
+  ## recent time step, given the way network.extract.with.lasttoggle is implemented
+  wtk4 <- logical(NROW(nwlt4))
+  for(i in seq_along(wtk4)) {
+    wtk4[i] <- any(nwel4[,1] == nwlt4[i,1] & nwel4[,2] == nwlt4[i,2]) || nwlt4[i,3] == 4
+  }
+  nwlt4 <- nwlt4[wtk4,,drop=FALSE]
+  expect_identical(nwlt4[order(nwlt4[,1], nwlt4[,2]),,drop=FALSE], nwdlt4[order(nwdlt4[,1], nwdlt4[,2]),,drop=FALSE])  
+
+  wtk5 <- logical(NROW(nwlt5))
+  for(i in seq_along(wtk5)) {
+    wtk5[i] <- any(nwel5[,1] == nwlt5[i,1] & nwel5[,2] == nwlt5[i,2]) || nwlt5[i,3] == 5
+  }
+  nwlt5 <- nwlt5[wtk5,,drop=FALSE]
+  expect_identical(nwlt5[order(nwlt5[,1], nwlt5[,2]),,drop=FALSE], nwdlt5[order(nwdlt5[,1], nwdlt5[,2]),,drop=FALSE])  
 })
