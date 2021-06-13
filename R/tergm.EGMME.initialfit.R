160f491fe0e1d5058596f7f8cc3d92498ee0e672
chad-klumb <cklumb@gmail.com>
2021-06-12 17:05:11 -0700

    flip diss signs in initialfit
    and change some names

--- R/tergm.EGMME.initialfit.R
+++ R/tergm.EGMME.initialfit.R
@@ -20,11 +20,11 @@ tergm.EGMME.initialfit<-function(init, nw, model, formula, model.mon, formula.mo
     fd.formulae <- .extract.fd.formulae(formula)
     
     form <- fd.formulae$form
-    diss <- fd.formulae$pers
+    pers <- fd.formulae$pers
     nonsep <- fd.formulae$nonsep
     
     model.form <- ergm_model(form, nw=nw, dynamic=TRUE, term.options=control$term.options)
-    model.diss <- ergm_model(diss, nw=nw, dynamic=TRUE, term.options=control$term.options)
+    model.pers <- ergm_model(pers, nw=nw, dynamic=TRUE, term.options=control$term.options)
     model.nonsep <- ergm_model(nonsep, nw=nw, dynamic=TRUE, term.options=control$term.options)
     
     ## non-separable terms are not currently allowed in the EDA
@@ -32,35 +32,38 @@ tergm.EGMME.initialfit<-function(init, nw, model, formula, model.mon, formula.mo
       stop("No initial parameter method for specified model and targets combination is implemented. Specify via control$init.")    
     }
     
-    wf <- which(grepl("^Form~", model$coef.names) | grepl("^offset\\(Form~", model$coef.names))
-    wd <- which(grepl("^Persist~", model$coef.names) | grepl("^offset\\(Persist~", model$coef.names) | grepl("^Diss~", model$coef.names) | grepl("^offset\\(Diss~", model$coef.names))
+    wf <- grepl("^Form~", model$coef.names) | grepl("^offset\\(Form~", model$coef.names)
+    wp <- grepl("^Persist~", model$coef.names) | grepl("^offset\\(Persist~", model$coef.names)
+    wd <- grepl("^Diss~", model$coef.names) | grepl("^offset\\(Diss~", model$coef.names)
+    
+    init[wd] <- -init[wd]
     
     init.form <- init[wf]
-    init.diss <- init[wd]
+    init.pers <- init[wp | wd]
     
     names(init.form) <- model.form$coef.names
-    names(init.diss) <- model.diss$coef.names
+    names(init.pers) <- model.pers$coef.names
     
     form <- nonsimp_update.formula(form, nw ~ ., from.new = "nw")
     model.form$formula <- form
   
     if(all(model.form$coef.names[!model.form$etamap$offsettheta] %in% .do(model.mon$coef.names))
              && (
-                  all(model.diss$etamap$offsettheta)
+                  all(model.pers$etamap$offsettheta)
                   || (
-                       length(model.diss$coef.names) == 1
-                       && .do(model.diss$coef.names) == "edges"
+                       length(model.pers$coef.names) == 1
+                       && .do(model.pers$coef.names) == "edges"
                        && "mean.age" %in% .do(model.mon$coef.names)
                      )
                 )
-             && all(.do(model.diss$coef.names) %in% model.form$coef.names)
-             && is.dyad.independent(model.diss)) {
+             && all(.do(model.pers$coef.names) %in% model.form$coef.names)
+             && is.dyad.independent(model.pers)) {
       if(verbose) message("Formation statistics are analogous to targeted statistics, dissolution is fixed or is edges with a mean.age target, dissolution terms appear to have formation analogs, and dissolution process is dyad-independent, so using edges dissolution approximation  (Carnegie et al.).")
       
-      if(!all(model.diss$etamap$offsettheta)){ # This must mean that the two provisos above are satisfied.
+      if(!all(model.pers$etamap$offsettheta)){ # This must mean that the two provisos above are satisfied.
         mean.age <- model.mon$target.stats[.do(model.mon$coef.names)=="mean.age"]
-        init.diss <- log(mean.age+1)
-        names(init.diss) <- "edges"
+        init.pers <- log(mean.age+1)
+        names(init.pers) <- "edges"
       }
       
       form.offset.coef <- if(any(model.form$etamap$offsetmap)) init.form[model.form$etamap$offsetmap] else NULL
@@ -77,15 +80,16 @@ tergm.EGMME.initialfit<-function(init, nw, model, formula, model.mon, formula.mo
       # In case it's not obvious (it's not to me) what the following
       # does, it takes non-offset elements of init.form, then, from
       # those, it takes those elements that correspond (by name) to the
-      # dissolution coefficient names and decrements them by init.diss.
+      # dissolution coefficient names and decrements them by init.pers.
       #
       # Yes, I am also a little surprised that assigning to a
       # double-index works.
-      init.form[!model.form$etamap$offsettheta][match(.do(names(init.diss)),names(init.form[!model.form$etamap$offsettheta]))] <-
-        init.form[!model.form$etamap$offsettheta][match(.do(names(init.diss)),names(init.form[!model.form$etamap$offsettheta]))] - init.diss
+      init.form[!model.form$etamap$offsettheta][match(.do(names(init.pers)),names(init.form[!model.form$etamap$offsettheta]))] <-
+        init.form[!model.form$etamap$offsettheta][match(.do(names(init.pers)),names(init.form[!model.form$etamap$offsettheta]))] - init.pers
         
-      init[wd] <- init.diss
+      init[wp | wd] <- init.pers
       init[wf] <- init.form
+      init[wd] <- -init[wd]
     }else{
       stop("No initial parameter method for specified model and targets combination is implemented. Specify via control$init.")
     }
