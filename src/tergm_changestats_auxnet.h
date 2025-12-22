ebf72b5e36375a07a4e7412989143e468691a229
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2025-12-22 17:23:03 +1100

    No longer rely on the deprecated macro GET_AUX_STORAGE_NUM().

--- src/tergm_changestats_auxnet.h
+++ src/tergm_changestats_auxnet.h
@@ -27,7 +27,7 @@ MAP_TOGGLE_FN(map_toggle__discord_lt_net_Network){
 #define map_toggle_maxtoggles__intersect_lt_net_Network 1
 MAP_TOGGLE_FN(map_toggle__intersect_lt_net_Network){
   ModelTerm *mtp = auxnet->mtp;
-  GET_AUX_STORAGE_NUM(StoreTimeAndLasttoggle, dur_inf, 1);
+  GET_AUX_STORAGE(1, StoreTimeAndLasttoggle, dur_inf);
   // See comments for u__intersect_lt_net_Network() for explanation.
   if(dur_inf->ticktock){
       MAP_TOGGLE_PROPAGATE_IF(edgestate != JUST_CHANGED(dur_inf,tail,head));
